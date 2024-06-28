8da19576d4af36b50355005b3fd6a72c60e2e306
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-06-28 21:26:41 +0700

    FORWARD MESSAGE

--- ServerTerminal/Program.cs
+++ ServerTerminal/Program.cs
@@ -28,11 +28,32 @@ class Server
             string message;
             while ((message = await reader.ReadLineAsync()) != null)
             {
-                Console.WriteLine($"Receiver : {message}");
+                Console.WriteLine($"Sender : {message}");
+                await ToSender(message);
             }
         }
             
        client.Close();
+    }
+    
+    static async Task ToSender(string msg)
+    {
+        try
+        {
+            TcpClient senderClient = new TcpClient("192.168.1.101",5713);
+            NetworkStream senderStram = senderClient.GetStream();
+            StreamWriter receiveWriter = new StreamWriter(senderStram);
+
+            await receiveWriter.WriteLineAsync(msg);
+            await receiveWriter.FlushAsync();
+
+            receiveWriter.Close();
+            senderStram.Close();
+            senderClient.Close();
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
 }
