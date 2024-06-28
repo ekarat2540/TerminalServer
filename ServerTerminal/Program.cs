eee90d0f55147efd46d6138c5cdbea0930dd8436
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-06-28 19:01:23 +0700

    SET METHOD GetSender from sender

--- ServerTerminal/Program.cs
+++ ServerTerminal/Program.cs
@@ -3,16 +3,36 @@ using System.Net.Sockets;
 
 class Server
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         try
         {
-            TcpListener server = TcpListener(IPAddress.Any, 5713);
+            TcpListener server = new TcpListener(IPAddress.Any, 5713);
             server.Start();
             Console.WriteLine("Server is started on port 5713");
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+               _ = GetSender(client);
+            }
         }catch (Exception e)
         {
             Console.WriteLine(e.Message);
         }
     }
+    static async Task GetSender(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            string message;
+            while ((message = await reader.ReadLineAsync()) != null)
+            {
+                Console.WriteLine($"Receiver : {message}");
+            }
+        }
+            
+       client.Close();
+    }
+
 }
